445e401f7a6a385e62de0a570c72cfccee0702e1
Nada Galal <ngalal@ucsd.edu>
2021-05-17 01:28:24 +0200

    part2 ~complete

--- scripts/script.js
+++ scripts/script.js
@@ -34,3 +34,16 @@ document.getElementsByTagName('img')[0].onclick = function(){
 window.addEventListener('popstate', (event) => {
 	setState(true, event.state);
 });
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
